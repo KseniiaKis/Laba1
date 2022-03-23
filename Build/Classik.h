e193e157c54f0073a817f5f373f6978ffae77a6c
KseniiaKis <99728814+KseniiaKis@users.noreply.github.com>
2022-03-23 19:23:24 +0300

    Delete Build directory

--- Build/Classik.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "skiing.h"
-class TClassik :public TSkiing
-{
-	bool Nasechki;
-public:
-	int GetNasech(); void SetNasech(int Nas);
-};
--- Build/Free.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "skiing.h"
-class TFree :public TSkiing
-{
-public:
-	bool GetHole(); void SetHole(bool Hol);
-protected:
-	bool Hole;
-};
