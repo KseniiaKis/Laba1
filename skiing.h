2b7de9fc44a6b5216a8d97254a7386d06d1ac4d4
KseniiaKis <99728814+KseniiaKis@users.noreply.github.com>
2022-04-11 23:00:49 +0300

    Update skiing.h

--- skiing.h
+++ skiing.h
@@ -5,10 +5,10 @@ using namespace std;
 class TSkiing
 {
 public:
+	void Skiing(int a = 0, int b = 0);
 	int GetSize(); void SetSize(int s);
 	int Getrig(); void Setrig(int rig);
 protected:
 	int size;
 	int rigidity;
-
 };
